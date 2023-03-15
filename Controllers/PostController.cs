0b4c1dc6d298643f63dad9c27ea576f9b8f18b1b
Michael Rath <michaelrath0@gmail.com>
2023-03-15 15:14:08 -0700

    Seperated ForumController into 2 seperate controllers: RoomController and PostController

---
+++ Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using Forum_API_Provider.Services.ForumService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum_API_Provider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private IForumRepository repository;
+        public PostController(IForumRepository repository)
+        {
+            this.repository = repository;
+
+        }
+        [HttpGet]
+        [Route("GetAllPosts")]
+        public async Task<IActionResult> GetPosts()
+        {
+            return Ok(await repository.GetAllPosts());
+        }
+    }
+}
---
+++ Controllers/RoomController.cs
@@ -0,0 +1,24 @@
+﻿using Forum_API_Provider.Services.ForumService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum_API_Provider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private IForumRepository repository;
+        public RoomController(IForumRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        [Route("GetAllRooms")]
+        public async Task<IActionResult> GetRooms()
+        {
+            return Ok(await repository.GetAllRooms());
+        }
+    }
+}
